66a419fe4cfdcc19383a9adb2492233cbfb40df2
kantiboyinaAjay <ajaykantiboyina12389@gmail.com>
2025-01-02 12:30:13 +0530

    update

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ const dotenv = require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(cors());
 app.use("/", Router);
 
 mongoose
   .connect(
     process.env.Database_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
     console.log("Connected to Mongo DB");
